945b832126909e15c313f566f5965930bd21bd06
KushEzio <kushsri88@gmail.com>
2020-06-29 12:01:43 +0530

    Dynamic div using renderer

--- src/app/dynamic-div/dynamic-div.component.ts
+++ src/app/dynamic-div/dynamic-div.component.ts
@@ -1,27 +1,60 @@
-import { Component, OnInit, HostListener, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  OnDestroy,
+  Renderer2,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-div',
   templateUrl: './dynamic-div.component.html',
   styleUrls: ['./dynamic-div.component.css'],
 })
-export class DynamicDivComponent implements OnInit, OnDestroy {
+export class DynamicDivComponent implements OnInit, OnDestroy, AfterViewInit {
   numbers: number[];
+  count: number;
   @HostListener('window:scroll', ['$event'])
   onScroll(event) {
-    // console.log('i am scrolling');
-    const count = this.numbers.length;
-    const numbers = Array(10)
-      .fill(0)
-      .map((x, i) => i + count + 1);
-    // console.log(count, numbers);
-    this.numbers = [...this.numbers, ...numbers];
+    console.log('i am scrolling');
+    // const numbers = Array(10);
+    //   .fill(0)
+    //   .map((x, i) => i + count + 1);
+    // // console.log(count, numbers);
+    // this.numbers = [...this.numbers, ...numbers];
+    this.count += 1;
+
+    var mydiv = this.render.createElement('div');
+    this.render.addClass(mydiv, 'text-center');
+    this.render.addClass(mydiv, 'wrapper');
+    var mybutton = this.render.createElement('button');
+    this.render.addClass(mybutton, 'btn-primary');
+    this.render.addClass(mybutton, 'btn');
+    this.render.setAttribute(mybutton, 'myid', `${this.count}`);
+
+    this.render.listen(mybutton, 'click', (myevent) => {
+      // console.log(this.render.)
+      console.log(myevent.target.innerHTML);
+      let value = myevent.target.innerHTML.split(' ');
+      console.log(value[1]);
+      // debugger;
+      alert(`Button '${value[1]}' is clicked`);
+    });
+    var text = this.render.createText(`Button ${this.count}`);
+    this.render.appendChild(mybutton, text);
+    this.render.appendChild(mydiv, mybutton);
+    this.render.appendChild(this.ele.nativeElement, mydiv);
   }
 
-  constructor() {
-    this.numbers = Array(25)
+  constructor(private render: Renderer2, private ele: ElementRef) {
+    this.numbers = Array(21)
       .fill(0)
       .map((x, i) => i + 1);
+  }
+  ngAfterViewInit() {
+    this.count = this.numbers.length;
   }
 
   ngOnInit(): void {
